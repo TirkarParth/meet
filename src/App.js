7a5c084c20b13c4f2b1baceb94b09d1c00be01e4
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-07-21 17:25:51 +0200

    Add acceptance and end-to-end tests for Features 1 and 2
    - Created and wrote acceptance tests for Feature 1 of the Meet app, covering the core functionality as outlined in the Exercise instructions.
    - Developed end-to-end tests for Feature 2 to ensure complete functionality from start to finish, including user interactions and expected outcomes.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import './App.css';
-import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import './App.css';
+import { useState, useEffect } from 'react';
 import { extractLocations, getEvents } from './api';
 
-function App() {
+const App = () => {
   const [events, setEvents] = useState([]);
-  const [currentNOE] = useState(32); // Removed setCurrentNOE since it's unused
+  // eslint-disable-next-line no-unused-vars
+  const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState('See all cities');
 
-  const fetchData = useCallback(async () => {
+  const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities"
-      ? allEvents
-      : allEvents.filter(event => event.location === currentCity);
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
-  }, [currentCity, currentNOE]); // Add currentNOE to the dependency array
+  };
 
   useEffect(() => {
     fetchData();
-  }, [currentCity, fetchData]); // Add fetchData to the dependency array
+  }, [currentCity, currentNOE]);
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents />
-      <EventList events={events} />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE}></NumberOfEvents>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+      ></CitySearch>
+      <EventList events={events}></EventList>
     </div>
   );
-}
+};
 
 export default App;
