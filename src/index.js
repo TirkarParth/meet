d4ae55492d62905c5fd80737fe6ce6c46d7abfb7
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-07-21 21:49:15 +0200

    integrate Atatus monitoring in index.js
    - Imported Atatus in the index.js file to set up performance monitoring and error tracking.
    - Added the initialization and configuration code provided by Atatus for proper setup.
    - Configured Atatus using the unique configuration code to start monitoring the application.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('6c24d34500b24a66be5a7873e3340988').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
